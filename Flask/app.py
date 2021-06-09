2ddd874ff5bc2fbb605ae396a8d7778ac6a997e5
Alexandr Pavlenko <alcohollex2@gmail.com>
2021-06-09 20:10:45 +0300

    _

--- Flask/app.py
+++ Flask/app.py
@@ -8,3 +8,6 @@ app = Flask(__name__)
 app.config.from_mapping(SQLALCHEMY_DATABASE_URI=config.POSTGRE_URI)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run()
